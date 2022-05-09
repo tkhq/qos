29124968c4e02878bb81b86736cd67859a2f79fd
Jack Kearney <jack@turnkey.io>
2022-05-09 13:18:14 -0400

    Add qos-core-cli crate

--- qos-core-cli/src/main.rs
+++ qos-core-cli/src/main.rs
@@ -1,44 +1,39 @@
-// use std::env;
+use std::env;
 
-// use qos::{
-// 	client::Client,
-// 	io::SocketAddress,
-// 	protocol::{EchoRequest, ProtocolRequest},
-// 	server::Server,
-// };
+use qos_core::{
+	client::Client,
+	io::SocketAddress,
+	protocol::{EchoRequest, ProtocolRequest},
+	server::Server,
+};
 
 pub fn main() {
-	println!("Hello, world!");
-	// let args: Vec<String> = env::args().collect();
+	let args: Vec<String> = env::args().collect();
 
-	// match args.get(1).map(|a| a.as_str()) {
-	// 	Some("server") => run_server(),
-	// 	Some("client") => run_client(),
-	// 	Some(_) | None => println!("Unknown command..."),
-	// };
+	match args.get(1).map(|a| a.as_str()) {
+		Some("server") => run_server(),
+		Some("client") => run_client(),
+		Some(_) | None => println!("Unknown command..."),
+	};
 }
 
-// fn run_client() {
-// 	let addr = SocketAddress::Unix(
-// 		nix::sys::socket::UnixAddr::new("./dev.sock").unwrap(),
-// 	);
-// 	let client = Client::new(addr);
-// 	let data = b"Hello, world!".to_vec();
-// 	let request = ProtocolRequest::Echo(EchoRequest { data });
-// 	let response = client.send(request).unwrap();
-// 	match response {
-// 		ProtocolRequest::Echo(er) => {
-// 			println!("{}", String::from_utf8(er.data).unwrap());
-// 		}
-// 		_ => {
-// 			println!("Unhandled...")
-// 		}
-// 	}
-// }
+fn run_client() {
+	let addr = SocketAddress::new_unix("./dev.sock");
+	let client = Client::new(addr);
+	let data = b"Hello, world!".to_vec();
+	let request = ProtocolRequest::Echo(EchoRequest { data });
+	let response = client.send(request).unwrap();
+	match response {
+		ProtocolRequest::Echo(er) => {
+			println!("{}", String::from_utf8(er.data).unwrap());
+		}
+		_ => {
+			println!("Unhandled...")
+		}
+	}
+}
 
-// fn run_server() {
-// 	let addr = SocketAddress::Unix(
-// 		nix::sys::socket::UnixAddr::new("./dev.sock").unwrap(),
-// 	);
-// 	Server::listen(addr).unwrap();
-// }
+fn run_server() {
+	let addr = SocketAddress::new_unix("./dev.sock");
+	Server::listen(addr).unwrap();
+}
---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+#![forbid(unsafe_code)]
+
+// pub mod client;
+// pub mod io;
+// pub mod protocol;
+// pub mod server;
