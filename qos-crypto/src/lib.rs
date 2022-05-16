771c453ed57aa4c7cb67922d65988522f14735c1
emostov <32168567+emostov@users.noreply.github.com>
2022-05-16 15:02:05 -0700

    Impl `RsaPair` basics

--- qos-crypto/src/lib.rs
+++ qos-crypto/src/lib.rs
@@ -28,24 +28,6 @@ impl From<openssl::error::ErrorStack> for CryptoError {
 	}
 }
 
-pub fn load_public_key(path: String) -> Result<Rsa<Public>, CryptoError> {
-	let mut content = Vec::new();
-	let mut file = File::open(path)?;
-	file.read_to_end(&mut content)?;
-	let pk = Rsa::public_key_from_pem(&content[..])?;
-	Ok(pk)
-}
-
-pub fn write_public_key(
-	path: String,
-	key: Rsa<Public>,
-) -> Result<(), CryptoError> {
-	let bytes = key.public_key_to_pem()?;
-	let mut file = File::create(path)?;
-	file.write_all(&bytes)?;
-	Ok(())
-}
-
 pub fn sign_with_key(
 	key: Rsa<Private>,
 	message: Vec<u8>,
@@ -57,20 +39,67 @@ pub fn sign_with_key(
 	Ok(signature)
 }
 
+/// RSA Private key pair.
+pub struct RsaPair {
+	private_key: Rsa<Private>,
+}
+
+impl RsaPair {
+	pub fn from_pem_file<P: AsRef<Path>>(path: P) -> Result<Self, CryptoError> {
+		let mut content = Vec::new();
+		let mut file = File::open(path)?;
+		file.read_to_end(&mut content)?;
+
+		Ok(Self { private_key: Rsa::private_key_from_pem(&content[..])? })
+	}
+
+	/// Sign the sha256 digest of `msg`. Returns the signature as a byte vec.
+	pub fn sign_sha256(&self, msg: &mut [u8]) -> Result<Vec<u8>, CryptoError> {
+		let pkey = PKey::from_rsa(self.private_key.clone())?;
+		let mut signer = Signer::new(MessageDigest::sha256(), &pkey)?;
+		signer.update(msg)?;
+		signer.sign_to_vec().map_err(Into::into)
+	}
+
+	pub fn public_key_pem(&self) -> Result<Vec<u8>, CryptoError> {
+		self.private_key.public_key_to_pem().map_err(Into::into)
+	}
+}
+
+impl TryFrom<PKey<Private>> for RsaPair {
+	type Error = CryptoError;
+	fn try_from(private_key: PKey<Private>) -> Result<Self, Self::Error> {
+		Ok(Self { private_key: private_key.rsa()? })
+	}
+}
+
 pub struct RsaPub {
 	pub_key: Rsa<Public>,
 }
 
 impl RsaPub {
-	pub fn from_pem_file<P>(path: P) -> Result<Self, CryptoError>
-	where
-		P: AsRef<Path>,
-	{
+	pub fn from_pem_file<P: AsRef<Path>>(path: P) -> Result<Self, CryptoError> {
 		let mut content = Vec::new();
 		let mut file = File::open(path)?;
 		file.read_to_end(&mut content)?;
 
-		Ok(Self { pub_key: Rsa::public_key_from_pem(&content[..])? })
+		Self::from_pem(&content[..])
+
+		// Ok(Self { pub_key: Rsa::public_key_from_pem(&content[..])? })
+	}
+
+	pub fn from_pem(pem: &[u8]) -> Result<Self, CryptoError> {
+		Ok(Self { pub_key: Rsa::public_key_from_pem(pem)? })
+	}
+
+	pub fn write_pem_file<P: AsRef<Path>>(
+		&self,
+		path: P,
+	) -> Result<(), CryptoError> {
+		let bytes = self.pub_key.public_key_to_pem()?;
+		let mut file = File::create(path)?;
+		file.write_all(&bytes)?;
+		Ok(())
 	}
 
 	pub fn verify_sha256(
@@ -115,12 +144,20 @@ mod test {
 
 	#[test]
 	fn rsa_pub_from_pem_file() {
-		// cur-dir/mock/rsa_public.mock.pem
 		let mut path = std::env::current_dir().unwrap();
 		path.push("mock");
 		path.push("rsa_public.mock.pem");
 
 		let _pub_key = RsaPub::from_pem_file(path.clone()).unwrap();
+	}
+
+	#[test]
+	fn rsa_pair_from_pem_file() {
+		let mut path = std::env::current_dir().unwrap();
+		path.push("mock");
+		path.push("rsa_private.mock.pem");
+
+		let _pair = RsaPair::from_pem_file(path.clone()).unwrap();
 	}
 
 	#[test]
@@ -140,5 +177,22 @@ mod test {
 
 		let rsa_pub: RsaPub = pair.clone().try_into().unwrap();
 		assert!(rsa_pub.verify_sha256(&signature, data).unwrap());
+	}
+
+	#[test]
+	fn e2e_crypto_from_pem_file() {
+		let mut path = std::env::current_dir().unwrap();
+		path.push("mock");
+		path.push("rsa_private.mock.pem");
+
+		let pair = RsaPair::from_pem_file(path.clone()).unwrap();
+
+		let msg = &mut b"vape nation".to_vec()[..];
+
+		let signature = pair.sign_sha256(msg).unwrap();
+
+		let pub_pem = pair.public_key_pem().unwrap();
+		let rsa_pub: RsaPub = RsaPub::from_pem(&pub_pem[..]).unwrap();
+		assert!(rsa_pub.verify_sha256(&signature, msg).unwrap());
 	}
 }
