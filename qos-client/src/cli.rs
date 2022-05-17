230e9cc05a1c9a50c38a376180d57bbea8eb2626
emostov <32168567+emostov@users.noreply.github.com>
2022-05-17 12:01:53 -0700

    Very ugly attestation doc sig verification

--- qos-client/src/cli.rs
+++ qos-client/src/cli.rs
@@ -110,6 +110,11 @@ impl CLI {
 }
 
 mod handlers {
+	use openssl::{
+		bn::BigNumContext,
+		ec::{EcGroup, EcKey, EcPoint},
+		nid::Nid,
+	};
 	use qos_core::protocol::{NsmRequest, NsmResponse};
 
 	use super::*;
@@ -160,18 +165,102 @@ mod handlers {
 
 		match response {
 			ProtocolMsg::NsmResponse(NsmResponse::Attestation { document }) => {
-				println!("{:?}", document);
+				use aws_nitro_enclaves_cose::CoseSign1;
 				use aws_nitro_enclaves_nsm_api::api::AttestationDoc;
+				// TODO: semantic verification from: https://github.com/aws/aws-nitro-enclaves-nsm-api/blob/main/docs/attestation_process.md
 
-				let doc = AttestationDoc::from_binary(&document[..]);
-				println!("doc={:?}", doc);
+				let cose_sign1 = CoseSign1::from_bytes(&document[..]).unwrap();
+				let encoded_document = cose_sign1.get_payload(None).unwrap();
+				let decoded_document =
+					AttestationDoc::from_binary(&encoded_document[..]).unwrap();
 
-				// TODO: delete this stuff
-				// use std::{fs::File, io::Write};
-				// let mut file =
-				// 	File::create("/home/tk/attest_document").unwrap();
-				// let output = format!("{:?}", document);
-				// file.write_all(&output.as_bytes()).unwrap();
+				let pcrs = decoded_document.pcrs;
+
+				let module = decoded_document.module_id;
+				// println!("module id: {:?}", module);
+
+				if let Some(public_key) = decoded_document.public_key {
+					// println!("public key: {:?}", public_key);
+				}
+
+				let cabundle = decoded_document.cabundle;
+				// println!("CA bundle: {:?}", cabundle);
+
+				// Target Certificate in
+				// let certificate = decoded_document.certificate;
+				// println!("Certificate: {:?}", certificate);
+
+				// let c = std::str::from_utf8(&certificate);
+				// println!("Certificate:");
+				// println!("{:?}", c);
+
+				//
+
+				// Trying to do... Certificate -> Public Key
+				// Certificate & CA bundle verification example: https://github.com/ppmag/aws-nitro-enclaves-attestation/blob/83ca87233298c302973a5bdbbb394c36cd7eb6e6/src/lib.rs#L233-L235
+				match x509_parser::parse_x509_certificate(
+					&decoded_document.certificate.clone(),
+				) {
+					Ok((remaining_input, certificate)) => {
+						assert!(
+							remaining_input.len() == 0,
+							"certificate was not valid DER encoding"
+						);
+
+						assert!(
+							certificate.tbs_certificate.version()
+								== x509_parser::x509::X509Version::V3,
+							"Wrong certificate version"
+						);
+
+						let extracted_key = {
+							let pub_key = certificate
+								.tbs_certificate
+								.subject_pki
+								.subject_public_key
+								.data;
+
+							let group =
+								EcGroup::from_curve_name(Nid::SECP384R1)
+									.unwrap();
+							let mut ctx = BigNumContext::new().unwrap();
+							let point =
+								EcPoint::from_bytes(&group, &pub_key, &mut ctx)
+									.unwrap();
+							let ec_key =
+								EcKey::from_public_key(&group, &point).unwrap();
+
+							openssl::pkey::PKey::try_from(ec_key).expect("EC Key could not be converted to open ssl primitive")
+						};
+
+						assert!(
+							cose_sign1.verify_signature(&extracted_key).unwrap(),
+							"Could not verify attestation document with target certificate"
+						);
+						println!("Verified att_doc target cert signature!")
+					}
+					Err(_) => panic!("Could not parse target certificate"),
+				}
+
+				// use openssl::ec::EcKey;
+				// let key = EcKey::public_key_from_der(&certificate);
+				// println!("Public Key: {:?}", key);
+
+				////
+				// Truths:
+				////
+
+				// 1. AWS Nitro Enclaves use ES384 algorithm to sign the document
+				// 2. Certificate is DER-encoded
+				//
+
+				// Verification Flow:
+				// 1. Check signature from the Certificate over the AttestationDocument
+				// 2. Verify the CA Bundle using the known root of trust and Certificate
+				//   - Assume ROT is known ahead of time
+				// 3. Business logic
+				//   - Is the application that is being run (as evidenced by the PCRs) the expected application to have possession of *this* key?
+				//   - (Human): How do I know that this build artifact is correct?
 			}
 			_ => panic!("Not an attestation response"),
 		}
