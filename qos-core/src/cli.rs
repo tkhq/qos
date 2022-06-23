79c5d33f91915f2d7a919af894f8ee69c2c165d6
emostov <32168567+emostov@users.noreply.github.com>
2022-06-22 21:21:19 -0700

    Try fix vm build

--- qos-core/src/cli.rs
+++ qos-core/src/cli.rs
@@ -50,7 +50,10 @@ impl EnclaveOptions {
 			self.parsed.single(USOCK),
 		) {
 			#[cfg(feature = "vm")]
-			(Some(c), Some(p), None) => SocketAddress::new_vsock(c, p),
+			(Some(c), Some(p), None) => SocketAddress::new_vsock(
+				c.parse::<u32>().unwrap(),
+				p.parse::<u32>().unwrap()
+			),
 			#[cfg(feature = "local")]
 			(None, None, Some(u)) => SocketAddress::new_unix(u),
 			_ => panic!("Invalid socket options"),
