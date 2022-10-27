3de73b9e69a4cd2e54603074c5483fd9eb0e2fd3
Zeke Mostov <zeke@turnkey.io>
2022-10-26 20:35:13 -0400

    Working with aes-gcm

--- src/qos_p256/src/main.rs
+++ src/qos_p256/src/main.rs
@@ -6,8 +6,14 @@ use p256::{ecdh::EphemeralSecret, EncodedPoint, PublicKey};
 use rand::prelude::*;
 use rand_core::OsRng;
 use sha2::Digest;
+use aes_gcm::{
+	aead::{Aead, KeyInit, OsRng as AesOsRng},
+	Aes256Gcm, Nonce // Or `Aes128Gcm`
+};
 
 type Aes256Cbc = Cbc<Aes256, Pkcs7>;
+
+const MESSAGE: &[u8] = b"plaintext super secret message";
 
 fn generate_pair() -> (EncodedPoint, PublicKey, EphemeralSecret) {
 	let private = EphemeralSecret::random(&mut OsRng);
@@ -16,6 +22,17 @@ fn generate_pair() -> (EncodedPoint, PublicKey, EphemeralSecret) {
 		.expect("Bob's public key invalid");
 
 	(encoded_point, public, private)
+}
+
+struct AdditionalAuthenticatedData {
+	nonce: Vec<u8>,
+	// send_pub_key: Vec<u8>,
+	pub_key_one: Vec<u8>,
+
+}
+
+struct Envelope {
+	additional_authenticated_data: AdditionalAuthenticatedData,
 }
 
 fn main() {
@@ -32,16 +49,14 @@ fn main() {
 
 	// Bob generates a shared AES key from this shared secret with the shared IV
 	let bob_shared_key = sha2::Sha512::digest(bob_shared_secret.raw_secret_bytes()).to_vec();
-	let bob_shared_cipher =
-		Aes256Cbc::new_from_slices(&bob_shared_key[..32], &iv).unwrap();
+	let bob_cipher = Aes256Gcm::new_from_slice(&bob_shared_key[..32]).unwrap();
 
-	// Bob encrypts a secret to Alice with the shared AES key
-	let message_str = String::from("Secret message");
-	let message = message_str.as_bytes();
-	let mut message_buffer = [0u8; 128];
-	message_buffer[..message.len()].copy_from_slice(message);
-	let encrypted_message =
-		bob_shared_cipher.encrypt(&mut message_buffer, message.len()).unwrap();
+	let random_bytes = rand::thread_rng().gen::<[u8; 32]>();
+	let hash = sha2::Sha512::digest(random_bytes).to_vec();
+	let nonce = 	Nonce::from_slice(&hash[..12]);
+	let encrypted_message = bob_cipher.encrypt(nonce, MESSAGE).unwrap();
+
+
 
 	// Alice derives the same shared secret as Bob
 	let alice_shared_secret = alice_private.diffie_hellman(&bob_public);
@@ -49,13 +64,11 @@ fn main() {
 	// Alice generates the same AES key as Bob from this shared secret and the
 	// shared IV
 	let alice_shared_key = sha2::Sha512::digest(alice_shared_secret.raw_secret_bytes()).to_vec();
-	let alice_shared_cipher =
-		Aes256Cbc::new_from_slices(&alice_shared_key[..32], &iv).unwrap();
+
+	let alice_cipher = Aes256Gcm::new_from_slice(&alice_shared_key[..32]).unwrap();
+	let decrypted_message = alice_cipher.decrypt(nonce, &encrypted_message[..]).unwrap();
 
 	// Alice decrypts Bob's message
-	let mut encrypted_message_vec = encrypted_message.to_vec();
-	let decrypted_message =
-		alice_shared_cipher.decrypt(&mut encrypted_message_vec).unwrap();
 
 	println!(
 		"\nAlice public key {:x?}",
@@ -65,9 +78,9 @@ fn main() {
 		"\nBob public key {:x?}",
 		qos_hex::encode(bob_private_ep.as_ref())
 	);
-	println!("\nEncrypted message: {:?}", qos_hex::encode(encrypted_message));
+	println!("\nEncrypted message: {:?}", qos_hex::encode(&encrypted_message));
 	println!(
 		"\nDecrypted message: {:?}",
-		str::from_utf8(decrypted_message).unwrap()
+		str::from_utf8(&decrypted_message).unwrap()
 	);
 }
