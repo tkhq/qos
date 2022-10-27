7aa30d9cee6e08fad7f9569ff9fb62176223b2b7
Zeke Mostov <zeke@turnkey.io>
2022-10-26 22:13:34 -0400

    Basic refactor

--- src/qos_p256/src/lib.rs
+++ src/qos_p256/src/lib.rs
@@ -12,18 +12,24 @@ use sha2::Digest;
 
 const AES256_KEY_LEN: usize = 32;
 
+fn create_cipher(private: &EphemeralSecret, public: &PublicKey) -> Aes256Gcm {
+	let shared_secret = private.diffie_hellman(public);
+	let shared_key = sha2::Sha512::digest(shared_secret.raw_secret_bytes());
+	Aes256Gcm::new_from_slice(&shared_key[..AES256_KEY_LEN]).unwrap()
+}
+
 #[derive(
 	Debug, borsh::BorshSerialize, borsh::BorshDeserialize, Clone, PartialEq,
 )]
 struct Envelope {
 	nonce: Vec<u8>,
 	/// Public key as sec1 encoded point with no compression
-	ephemeral_pub_key: Vec<u8>,
+	ephemeral_public: Vec<u8>,
 	/// The data encrypted to the symmetrical key
 	encrypted_message: Vec<u8>,
 }
 
-struct P256Pair {
+pub struct P256Pair {
 	private: EphemeralSecret,
 }
 
@@ -36,50 +42,45 @@ impl P256Pair {
 	/// Decrypt a message encoded to this pair's public key.
 	// TODO: make this fallible and remove panics.
 	pub fn decrypt(&self, serialized_envelope: &[u8]) -> Vec<u8> {
-		let Envelope { nonce, ephemeral_pub_key, encrypted_message } =
+		let Envelope { nonce, ephemeral_public, encrypted_message } =
 			Envelope::try_from_slice(serialized_envelope).unwrap();
 
 		let nonce = Nonce::from_slice(&nonce);
-		let ephemeral_pub_key =
-			PublicKey::from_sec1_bytes(&ephemeral_pub_key).unwrap();
+		let ephemeral_public =
+			PublicKey::from_sec1_bytes(&ephemeral_public).unwrap();
 
-		let shared_secret = self.private.diffie_hellman(&ephemeral_pub_key);
-		let shared_key = sha2::Sha512::digest(shared_secret.raw_secret_bytes());
-		let cipher =
-			Aes256Gcm::new_from_slice(&shared_key[..AES256_KEY_LEN]).unwrap();
+		let cipher = create_cipher(&self.private, &ephemeral_public);
 
 		cipher.decrypt(nonce, &*encrypted_message).unwrap()
 	}
 }
 
-struct P256Public {
+pub struct P256Public {
 	public_key: PublicKey,
 }
 
 impl P256Public {
 	// TODO: make this fallible and remove panics.
 	pub fn encrypt(&self, message: &[u8]) -> Vec<u8> {
-		let eph_private = EphemeralSecret::random(&mut OsRng);
-		let eph_public_key = eph_private.public_key();
+		let ephemeral_private = EphemeralSecret::random(&mut OsRng);
+		let ephemeral_public = ephemeral_private.public_key();
 
-		let shared_secret = eph_private.diffie_hellman(&self.public_key);
-		let shared_key = sha2::Sha512::digest(shared_secret.raw_secret_bytes());
-		let cipher =
-			Aes256Gcm::new_from_slice(&shared_key[..AES256_KEY_LEN]).unwrap();
+		let cipher = create_cipher(&ephemeral_private, &self.public_key);
 
 		let nonce = {
 			let random_bytes = rand::thread_rng().gen::<[u8; 32]>();
 			*Nonce::from_slice(&random_bytes[..12])
 		};
 
-		// TODO: use nonce||ephemeral_pub_key as authenticated data .. although
+		// TODO: use nonce||ephemeral_public as authenticated data .. although
 		// doesn't seem strictly necessary
 		let encrypted_message = cipher.encrypt(&nonce, message).unwrap();
 
 		let envelope = Envelope {
 			encrypted_message,
 			nonce: nonce.to_vec(),
-			ephemeral_pub_key: eph_public_key
+			ephemeral_public: ephemeral_public
+				// TODO: Can we do compression? Does it matter?
 				.to_encoded_point(false)
 				.as_ref()
 				.to_vec(),
@@ -87,6 +88,8 @@ impl P256Public {
 
 		envelope.try_to_vec().unwrap()
 	}
+
+	// TODO: from der/sec1 etc
 }
 
 impl From<&P256Pair> for P256Public {
