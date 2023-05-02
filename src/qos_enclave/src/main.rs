40484ea83a3bb01464fd8dec674d1f3e97545171
Lance R. Vick <lance@lrvick.net>
2023-05-01 19:37:36 -0700

    qos_enclave w/ env var config and http health check

--- src/qos_enclave/src/main.rs
+++ src/qos_enclave/src/main.rs
@@ -1,15 +1,30 @@
-use std::os::unix::net::UnixStream;
-
-use nitro_cli::common::commands_parser::RunEnclavesArgs;
-use nitro_cli::common::{
-    enclave_proc_command_send_single, logger
+use std::{
+    os::unix::net::UnixStream,
+    thread,
+    io::Write,
+    net::{TcpListener, Shutdown},
 };
-use nitro_cli::common::{EnclaveProcessCommandType, ExitGracefully};
-use nitro_cli::enclave_proc_comm::enclave_proc_spawn;
+use nitro_cli::{
+    enclave_proc_comm::enclave_proc_spawn,
+    common::{
+        commands_parser::RunEnclavesArgs,
+        enclave_proc_command_send_single,
+        logger,
+        EnclaveProcessCommandType,
+        ExitGracefully
+    }
+};
 
 const RUN_ENCLAVE_STR: &str = "Run Enclave";
 
 fn main() {
+
+    let eif_path = std::env::var("EIF_PATH").unwrap_or("/nitro.eif".to_string());
+    let enclave_cid = std::env::var("ENCLAVE_CID").unwrap_or("16".to_string());
+    let memory_mib = std::env::var("MEMORY_MIB").unwrap_or("1024".to_string());
+    let cpu_count = std::env::var("CPU_COUNT").unwrap_or("2".to_string());
+    let enclave_name = std::env::var("ENCLAVE_NAME").unwrap_or("nitro".to_string());
+
     let logger = logger::init_logger()
         .map_err(|e| e.set_action("Logger initialization".to_string()))
         .ok_or_exit_with_errno(None);
@@ -28,15 +43,18 @@ fn main() {
         .ok_or_exit_with_errno(None);
 
     let run_args = RunEnclavesArgs {
-        eif_path: String::from("/home/lrvick/qos/out/aws-x86_64.eif"),
-        enclave_cid: None,
-        memory_mib: 512,
+        eif_path: eif_path,
+        enclave_cid: Some(enclave_cid.parse::<u64>().unwrap()),
+        memory_mib: memory_mib.parse::<u64>().unwrap(),
         cpu_ids: None,
         debug_mode: false,
         attach_console: false,
-        cpu_count: Some(2),
-        enclave_name: Some(String::from("nitro")),
+        cpu_count: Some(cpu_count.parse::<u32>().unwrap()),
+        enclave_name: Some(enclave_name),
     };
+
+    println!("Booting Nitro Enclave:");
+    println!("{:?}",run_args);
 
     enclave_proc_command_send_single(
         EnclaveProcessCommandType::Run,
@@ -50,4 +68,20 @@ fn main() {
     .ok_or_exit_with_errno(None);
 
     replies.push(socket);
+
+    //TODO: This is maybe a lie. We should implement a describe-enclaves here
+    println!("Nitro socket connected.");
+
+    println!("Starting health service");
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+    for stream in listener.incoming() {
+        thread::spawn(move || {
+            let mut stream = stream.unwrap();
+            match stream.write(b"HTTP/1.1 200 OK\r\r\n\r") {
+                Ok(_) => println!("Health response sent"),
+                Err(e) => println!("Failed sending health response: {}!", e),
+            }
+            stream.shutdown(Shutdown::Write).unwrap();
+        });
+    }
 }
