d76154bab23d95f1a6a498ada3f5e91eab22f217
Andrew Min <andrew@turnkey.io>
2024-07-03 00:33:07 -0400

    minor typos

--- src/qos_host/src/cli.rs
+++ src/qos_host/src/cli.rs
@@ -55,7 +55,7 @@ impl GetParserForOptions for HostParser {
 					.takes_value(true)
 			)
 			.token(
-				Token::new(VSOCK_TO_HOST, "wether to add the to-host svm flag to the enclave vsock connection. Valid options are `true` or `false`")
+				Token::new(VSOCK_TO_HOST, "whether to add the to-host svm flag to the enclave vsock connection. Valid options are `true` or `false`")
 					.takes_value(true)
 					.required(false)
 					.forbids(vec![USOCK])
