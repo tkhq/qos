3727f35df05036242b8249010135050ee6d59bac
Louis Thibault <louis@turnkey.io>
2024-09-24 14:48:10 -0400

    Fix typo in docstring.

--- src/qos_client/src/cli/services.rs
+++ src/qos_client/src/cli/services.rs
@@ -2045,7 +2045,7 @@ pub(crate) fn dangerous_dev_boot<P: AsRef<Path>>(
 	let pivot = fs::read(&pivot_path).expect("Failed to read pivot binary.");
 
 	let mock_pcr = vec![0; 48];
-	// Create a manifest with manifest set of 1 - everything hardcoded expect
+	// Create a manifest with manifest set of 1 - everything hardcoded except
 	// pivot config
 	let manifest = Manifest {
 		namespace: Namespace {
