a4cc07be92f43ce118bac9899395634196626aea
Arnaud Brousseau <rno@turnkey.io>
2025-02-27 12:57:11 -0600

    Fix qos_net's CLI to build correctly without default features

--- src/qos_net/src/main.rs
+++ src/qos_net/src/main.rs
@@ -1,5 +1,14 @@
+#[cfg(feature = "proxy")]
 use qos_net::cli::CLI;
 
+#[cfg(feature = "proxy")]
 pub fn main() {
 	CLI::execute();
 }
+
+
+#[cfg(not(feature = "proxy"))]
+pub fn main() {
+	panic!("Cannot run qos_net CLI without proxy feature enabled")
+}
+
