94f117ce6520a867d44ffd30066657e37f5229f0
Ales Katona <ales@turnkey.io>
2025-04-24 13:35:37 -0700

    implement async qos_net::AsyncProxy
    this includes:
     - Reaper::execute_async [qos_core]
     - AsyncStream [qos_core]
     - AsyncPool/AsyncStream [qos_core]
     - AsyncListener [qos_core]
     - AsyncRequestProcessor [qos_core]
     - AsyncProxyConnection [qos_net]
     - AsyncProxy [qos_net]
     - AsyncProxyStream [qos_net]
     - AsyncHostServer [qos_host]
     - integration tests additions
    
    as well as fixing qos_next's qos_core "vm" feature dependency issue
    by only requiring it if qos_net is requested with it

--- src/integration/src/lib.rs
+++ src/integration/src/lib.rs
@@ -28,7 +28,12 @@ pub const PIVOT_PANIC_PATH: &str = "../target/debug/pivot_panic";
 /// Path to an enclave app that has routes to test remote connection features.
 pub const PIVOT_REMOTE_TLS_PATH: &str = "../target/debug/pivot_remote_tls";
 /// Path to an enclave app that has routes to test remote connection features.
+pub const PIVOT_ASYNC_REMOTE_TLS_PATH: &str =
+	"../target/debug/pivot_async_remote_tls";
+/// Path to an enclave app that has routes to test remote connection features.
 pub const QOS_NET_PATH: &str = "../target/debug/qos_net";
+/// Path to an enclave app that has routes to test async remote connection features.
+pub const ASYNC_QOS_NET_PATH: &str = "../target/debug/async_qos_net";
 /// Path to an enclave app that has routes to stress our socket.
 pub const PIVOT_SOCKET_STRESS_PATH: &str =
 	"../target/debug/pivot_socket_stress";
