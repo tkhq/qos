bd2f0f947cef81984a7b185ccfc7b7753dfa81a1
Ales Katona <ales@turnkey.io>
2025-07-22 16:15:20 -0700

    integration: remove duplicate tests and pivots

--- src/integration/src/bin/pivot_ok4.rs
+++
@@ -1,5 +0,0 @@
-use integration::PIVOT_OK4_SUCCESS_FILE;
-
-fn main() {
-	integration::Cli::execute(PIVOT_OK4_SUCCESS_FILE);
-}
--- src/integration/src/bin/pivot_ok5.rs
+++
@@ -1,5 +0,0 @@
-use integration::PIVOT_OK5_SUCCESS_FILE;
-
-fn main() {
-	integration::Cli::execute(PIVOT_OK5_SUCCESS_FILE);
-}
