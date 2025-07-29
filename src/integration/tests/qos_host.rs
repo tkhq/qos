94244a19f4bb328da0c3611fa1c8a2146af7674d
Ales Katona <ales@turnkey.io>
2025-07-29 12:23:49 -0700

    integration: add interleaving_socket_stress test

--- src/integration/tests/qos_host.rs
+++ src/integration/tests/qos_host.rs
@@ -3,10 +3,13 @@ use std::{process::Command, time::Duration};
 use integration::PIVOT_OK_PATH;
 use qos_test_primitives::{ChildWrapper, PathWrapper};
 
-const TEST_ENCLAVE_SOCKET: &str = "/tmp/qos_host_test.enclave.sock";
+const TEST_ENCLAVE_SOCKET: &str = "/tmp/async_qos_host_test/enclave.sock";
 
-#[test]
-fn connects_and_gets_info() {
+#[tokio::test]
+async fn connects_and_gets_info() {
+	// prep sock pool dir
+	std::fs::create_dir_all("/tmp/async_qos_host_test").unwrap();
+
 	let _qos_host: ChildWrapper = Command::new("../target/debug/qos_host")
 		.arg("--usock")
 		.arg(TEST_ENCLAVE_SOCKET)
@@ -20,18 +23,20 @@ fn connects_and_gets_info() {
 		.unwrap()
 		.into();
 
-	std::thread::sleep(Duration::from_millis(100)); // let the qos_host start
+	tokio::time::sleep(Duration::from_millis(100)).await; // let the qos_host start
 
 	let r = ureq::get("http://127.0.0.1:3323/qos/enclave-info").call();
 	assert!(r.is_err()); // expect 500 here
 
-	let secret_path: PathWrapper = "/tmp/qos_host_reaper_works.secret".into();
+	let enclave_socket = format!("{TEST_ENCLAVE_SOCKET}_0"); // manually pick the 1st one
+	let secret_path: PathWrapper = "./async_qos_host_test.secret".into();
 	// let eph_path = "reaper_works.eph.key";
-	let manifest_path: PathWrapper =
-		"/tmp/qos_host_reaper_works.manifest".into();
+	let manifest_path: PathWrapper = "async_qos_host_test.manifest".into();
 
 	// For our sanity, ensure the secret does not yet exist
 	drop(std::fs::remove_file(&*secret_path));
+	// Remove the socket file if it exists as well, in case of bad crashes
+	drop(std::fs::remove_file(&enclave_socket));
 
 	let mut _enclave_child_process: ChildWrapper =
 		Command::new("../target/debug/qos_core")
@@ -53,7 +58,7 @@ fn connects_and_gets_info() {
 			.into();
 
 	// Give the enclave server time to bind to the socket
-	std::thread::sleep(std::time::Duration::from_millis(500));
+	tokio::time::sleep(std::time::Duration::from_millis(200)).await;
 
 	let r = ureq::get("http://127.0.0.1:3323/qos/enclave-info").call();
 	assert!(r.is_ok()); // expect 200 here
