ea0b1dfcd585c0dcdfc796a80c85fe57b08edaf1
Zeke Mostov <zeke@turnkey.io>
2026-02-06 15:29:49 -0500

    Make test less flaky

--- src/integration/tests/host_bridge.rs
+++ src/integration/tests/host_bridge.rs
@@ -3,7 +3,7 @@ use std::{
 	process::Command,
 };
 
-use integration::{wait_for_usock, PIVOT_TCP_PATH};
+use integration::{wait_for_tcp_sock, wait_for_usock, PIVOT_TCP_PATH};
 use qos_core::io::{HostBridge, SocketAddress, Stream, StreamPool};
 use qos_test_primitives::{find_free_port, ChildWrapper};
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
@@ -16,30 +16,35 @@ async fn vsock_to_tcp_bridge_works() {
 	let host_addr: std::net::SocketAddr =
 		SocketAddrV4::new(Ipv4Addr::LOCALHOST, port).into();
 
-	HostBridge::new(pool, host_addr).vsock_to_tcp().await;
 	let mut pivot: ChildWrapper = Command::new(PIVOT_TCP_PATH)
 		.arg(format!("{port}"))
 		.spawn()
 		.unwrap()
 		.into();
 
+	HostBridge::new(pool, host_addr).vsock_to_tcp().await;
 	wait_for_usock(APP_USOCK).await;
 
 	let mut stream = Stream::new(&SocketAddress::new_unix(APP_USOCK));
 	let mut stream2 = Stream::new(&SocketAddress::new_unix(APP_USOCK));
+	// Wait for pivot to bind. This needs to be ready before the streams try
+	// to connect
+	wait_for_tcp_sock(&format!("127.0.0.1:{port}")).await;
 	stream.connect().await.unwrap();
 	stream2.connect().await.unwrap();
 
 	// send b"hello" and expect it back
 	assert_eq!(5, stream.write(b"hello").await.unwrap());
+
 	// send b"done" and expect it back with pivot exiting
 	assert_eq!(4, stream2.write(b"done").await.unwrap());
 
-	let mut buf = [0u8; 4];
-	assert_eq!(4, stream2.read(&mut buf).await.unwrap());
-
+	// Read hello
 	let mut buf = [0u8; 5];
 	assert_eq!(5, stream.read(&mut buf).await.unwrap());
+	// Read done
+	let mut buf = [0u8; 4];
+	assert_eq!(4, stream2.read(&mut buf).await.unwrap());
 
 	assert!(pivot.0.wait().unwrap().success());
 }
