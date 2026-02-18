813cd0190b23af4a2b75801e5b3531c33506c9fa
Zeke Mostov <zeke@turnkey.io>
2026-02-18 14:32:57 -0500

    feat: add version and sha to qos_host enclave info

---
+++ src/qos_client/build.rs
@@ -0,0 +1,17 @@
+#![allow(missing_docs)]
+
+use std::process::Command;
+
+fn main() {
+	let sha = Command::new("git")
+		.args(["rev-parse", "--short", "HEAD"])
+		.output()
+		.ok()
+		.filter(|o| o.status.success())
+		.and_then(|o| String::from_utf8(o.stdout).ok())
+		.map(|s| s.trim().to_string())
+		.unwrap_or_else(|| "unknown".to_string());
+
+	println!("cargo:rustc-env=GIT_SHA={sha}");
+	println!("cargo:rerun-if-changed=../.git/HEAD");
+}
